468b7cac8f08bc3b2468b974fb47fc94576d77a3
陶文猛 <15152806231@163.com>
2019-04-24 15:25:41 +0800

    分页查询

---
+++ src/cn/jdbc/test/TestJDBC_05.java
@@ -0,0 +1,65 @@
+package cn.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 分页查询
+ */
+public class TestJDBC_05 {
+	
+	public static void list(int start,int count){
+		/**
+		 * 初始化驱动
+		 */
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		/**
+		 * 创建与数据库的连接
+		 * 创建Statement
+		 */
+		try(
+			Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8","root","admin");
+			Statement s = c.createStatement();	
+				)
+		{
+			String sql = "select * from hero limit " +start + "," + count;
+			ResultSet rs = s.executeQuery(sql);
+			while(rs.next()){
+                int id = rs.getInt("id");// 可以使用字段名
+                String name = rs.getString(2);// 也可以使用字段的顺序
+                float hp = rs.getFloat("hp");
+                int damage = rs.getInt(4);
+                System.out.printf("%d\t%s\t%f\t%d%n", id, name, hp, damage);
+			}
+			
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		list(10,5);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
