5dc4e6b10c3cebe8cacf8f89c4dc14cdc4ea663a
陶文猛 <15152806231@163.com>
2019-04-24 14:44:23 +0800

    JDBC:增加

---
+++ src/cn/jdbc/test/TestJDBC_Add.java
@@ -0,0 +1,52 @@
+package cn.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ */
+public class TestJDBC_Add {
+	
+	public static void main(String[] args) {
+		/**
+		 * 初始化驱动
+		 */
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		/**
+		 * 创建与数据库的连接
+		 * 创建Statement
+		 */
+		try(
+			Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8","root","admin");
+			Statement s = c.createStatement();	
+				)
+		{
+            String sql = "insert into hero values(null," + "'粥粥'" + "," + 313.0f + "," + 50 + ")";
+            s.execute(sql);
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
